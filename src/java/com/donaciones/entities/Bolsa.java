58ee010dbac279dbb786a0754b0373f42cec6b4e
daniel865 <daniel865live@gmail.com>
2014-05-27 13:24:23 -0500

    new commit

--- src/java/com/donaciones/entities/Bolsa.java
+++ src/java/com/donaciones/entities/Bolsa.java
@@ -1,16 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.donaciones.entities;
 
 import java.sql.Date;
 
 /**
- *
- * @author daniel
+ * Clase encargda de crear las entidades de tipo Bolsa
+ * @author Daniel Rúa Madrid
+ * @version 1.0
+ * @since 20/05/2014
  */
 public class Bolsa {
     
@@ -24,6 +20,18 @@ public class Bolsa {
     private String observaciones;
     private String estado;
 
+    /**
+     * Constructor de la entidad Bolsa
+     * @param codigo Código de la bolsa de sangre
+     * @param codigo_jornada  Código de la jornada de donación
+     * @param grupo_sanguineo Grupo sanguineo de la bolsa de sangre
+     * @param rh Tipo de RH 
+     * @param fecha_donacion Fecha de donación de la bolsa de sangre
+     * @param fecha_vencimiento Fecha de vencimiento de la bolsa de sangre
+     * @param cantidad_sangre Cantidad de sangre de la bolsa
+     * @param observaciones Observaciones de la bolsa de sangre
+     * @param estado Estado de la bolsa de sangre
+     */
     public Bolsa(String codigo, String codigo_jornada, String grupo_sanguineo, String rh, Date fecha_donacion, Date fecha_vencimiento, int cantidad_sangre, String observaciones, String estado) {
         this.codigo = codigo;
         this.codigo_jornada = codigo_jornada;
@@ -36,74 +44,146 @@ public class Bolsa {
         this.estado = estado;
     }
 
+    /**
+     * Retorna un valor del estado
+     * @return Estado de la bolsa
+     */
     public String getEstado() {
         return estado;
     }
 
+    /**
+     * Asigna un valor al estado
+     * @param estado Valor del estado de la bolsa
+     */
     public void setEstado(String estado) {
         this.estado = estado;
     }
 
+    /**
+     * Retorna el código de la bolsa de sangre.
+     * @return Código de la bolsa
+     */
     public String getCodigo() {
         return codigo;
     }
 
+    /**
+     * Asigna un código a la bolsa
+     * @param codigo Código de la bolsa
+     */
     public void setCodigo(String codigo) {
         this.codigo = codigo;
     }
 
+    /**
+     * Retorna el código de la jornada de donación
+     * @return Código de la jornada de donación
+     */
     public String getCodigo_jornada() {
         return codigo_jornada;
     }
 
+    /**
+     * Asigna un valor al código de la jornada
+     * @param codigo_jornada Código de la jornada
+     */
     public void setCodigo_jornada(String codigo_jornada) {
         this.codigo_jornada = codigo_jornada;
     }
 
+    /**
+     * Retorna el grupo sanguineo de la bolsa de sangre
+     * @return Grupo sanguineo de la bolsa de sangre
+     */
     public String getGrupo_sanguineo() {
         return grupo_sanguineo;
     }
 
+    /**
+     * Asigna un grupo sanguineo a la bolsa de sangre
+     * @param grupo_sanguineo Grupo Sanguineo de la bolsa de sangre
+     */
     public void setGrupo_sanguineo(String grupo_sanguineo) {
         this.grupo_sanguineo = grupo_sanguineo;
     }
 
+    /**
+     * Retorna el RH de la bolsa de sangre
+     * @return Rh de la bolsa
+     */
     public String getRh() {
         return rh;
     }
 
+    /**
+     * Asigna un valor al RH de la bolsa
+     * @param rh Rh de la bolsa
+     */
     public void setRh(String rh) {
         this.rh = rh;
     }
 
+    /**
+     * Retorna la fecha de donacion
+     * @return Fecha de donación de la bolsa
+     */
     public Date getFecha_donacion() {
         return fecha_donacion;
     }
 
+    /**
+     * Asigna un valor a la fecha de donación
+     * @param fecha_donacion Feha de donación de la bolsa
+     */
     public void setFecha_donacion(Date fecha_donacion) {
         this.fecha_donacion = fecha_donacion;
     }
 
+    /**
+     * Retorna la fecha de vencimiento de la bolsa de sangre
+     * @return Fecha de Vencimiento de la bolsa
+     */
     public Date getFecha_vencimiento() {
         return fecha_vencimiento;
     }
 
+    /**
+     * Asigna un valor a la fecha de vencimiento
+     * @param fecha_vencimiento Fecha de Vencimiento de la bolsa
+     */
     public void setFecha_vencimiento(Date fecha_vencimiento) {
         this.fecha_vencimiento = fecha_vencimiento;
     }
 
+    /**
+     * Retorna la cantidad de sangre de la bolsa
+     * @return Cantidad de Sangre
+     */
     public int getCantidad_sangre() {
         return cantidad_sangre;
     }
 
+    /**
+     * Asigna una cantidad de sangre a la bolsa
+     * @param cantidad_sangre Cantidad de Sangre
+     */
     public void setCantidad_sangre(int cantidad_sangre) {
         this.cantidad_sangre = cantidad_sangre;
     }
 
+    /**
+     * Retorna las observaciones de la bolsa de sangre
+     * @return Observaciones de la bolsa
+     */
     public String getObservaciones() {
         return observaciones;
     }
 
+    /**
+     * Asigna las observaciones a la bolsa de sangre
+     * @param observaciones Observaciones de la bolsa
+     */
     public void setObservaciones(String observaciones) {
         this.observaciones = observaciones;
     }
