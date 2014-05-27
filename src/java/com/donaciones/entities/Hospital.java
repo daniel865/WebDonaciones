35953b6433d5b7180a83d6b4074d15f097eaf3fe
daniel865 <daniel865live@gmail.com>
2014-05-27 16:02:04 -0500

    new commit

--- src/java/com/donaciones/entities/Hospital.java
+++ src/java/com/donaciones/entities/Hospital.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.donaciones.entities;
 
 /**
- *
- * @author daniel
+ * Clase encargada de crear la entidad Hospital
+ * @author Daniel Rúa Madrid
+ * @version 1.0
+ * @since 20/05/2014
+ * 
  */
 public class Hospital {
     
