db2499bc331daf35ec9db4f856b0c1f888f050cf
Daniel <Daniel@daniel-pc>
2014-06-15 17:03:01 -0500

    new commit

--- src/java/com/donaciones/servlets/UsuarioServlet.java
+++ src/java/com/donaciones/servlets/UsuarioServlet.java
@@ -8,15 +8,22 @@ package com.donaciones.servlets;
 import com.donaciones.dao.Conexion;
 import com.donaciones.entities.Usuario;
 import com.donaciones.dao.UsuarioDAO;
+import java.io.File;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import net.sf.jasperreports.engine.JREmptyDataSource;
+import net.sf.jasperreports.engine.JasperRunManager;
 
 /**
  *
@@ -39,7 +46,7 @@ public class UsuarioServlet extends HttpServlet {
 
         String accion = request.getParameter("accion");
 
-        if (("Guardar").equals(accion)) {
+        if ("Guardar".equals(accion)) {
             UsuarioDAO usuarioDAO = new UsuarioDAO(new Conexion("dba_donaciones", "donaciones", "jdbc:mysql://localhost/bd_donaciones"));
             String nro_identificacion = request.getParameter("nro_identificacion");
             String nombres = request.getParameter("nombres");
@@ -68,111 +75,105 @@ public class UsuarioServlet extends HttpServlet {
                 request.setAttribute("estado", estado);
             }
             request.getRequestDispatcher("RegistrarUsuario.jsp").forward(request, response);
-        } else {
-            if (("Consultar").equals(accion)) {
-                UsuarioDAO usuarioDAO = new UsuarioDAO(new Conexion("dba_donaciones", "donaciones", "jdbc:mysql://localhost/bd_donaciones"));
-                String user = request.getParameter("buscar_usu");
-                Usuario usuario;
-                try {
-                    usuario = usuarioDAO.buscarUsuario(user);
-                    request.setAttribute("usuario", user);
-                    request.setAttribute("nro_identificacion", usuario.getNro_Identificacion());
-                    request.setAttribute("nombres", usuario.getNombres());
-                    request.setAttribute("apellido1", usuario.getApellido1());
-                    request.setAttribute("apellido2", usuario.getApellido2());
-                    request.setAttribute("usuario", usuario.getUsuario());
-                    request.setAttribute("correo", usuario.getCorreo());
-                    request.setAttribute("pass", usuario.getPassword());
-                    request.setAttribute("pass2", usuario.getPassword());
-                    request.setAttribute("perfil", usuario.getPerfil());
-                    request.setAttribute("estado", usuario.getEstado());
-                    request.setAttribute("mensaje", "El usuario fue encontrado");
-                    request.getRequestDispatcher("RegistrarUsuario.jsp").forward(request, response);
-                } catch (Exception ex) {
-                    Logger.getLogger(Usuario.class.getName()).log(Level.SEVERE, null, ex);
-                    request.setAttribute("mensaje", "El usuario no fue encontrado");
-                    request.getRequestDispatcher("RegistrarUsuario.jsp").forward(request, response);
-                }
-            } else {
-                if (("Consultar Usuario").equals(accion)) {
-                    UsuarioDAO usuarioDAO = new UsuarioDAO(new Conexion("dba_donaciones", "donaciones", "jdbc:mysql://localhost/bd_donaciones"));
-                    String user = request.getParameter("usuario2");
-                    Usuario usuario;
-                    try {
-                        usuario = usuarioDAO.buscarUsuario(user);
-                        request.setAttribute("nro_identificacion2", usuario.getNro_Identificacion());
-                        request.setAttribute("nombre2", usuario.getNombres());
-                        request.setAttribute("apellidos2", usuario.getApellido1());
-                        request.setAttribute("usuario2", usuario.getUsuario());
-                        request.setAttribute("correo2", usuario.getCorreo());
-                        request.setAttribute("pass3", usuario.getPassword());
-                        request.setAttribute("pass4", usuario.getPassword());
-                        request.setAttribute("perfil2", usuario.getPerfil());
-                        request.setAttribute("estado2", usuario.getEstado());
-                        request.getRequestDispatcher("RegistrarUsuario.jsp").forward(request, response);
-                    } catch (Exception ex) {
-                        Logger.getLogger(Usuario.class.getName()).log(Level.SEVERE, null, ex);
-                        request.setAttribute("mensaje", "El usuario no fue encontrado");
-                        request.getRequestDispatcher("RegistrarUsuario.jsp").forward(request, response);
-                    }
-
-                } else {
-                    if (("Modificar").equals(accion)) {
-                        UsuarioDAO usuarioDAO = new UsuarioDAO(new Conexion("dba_donaciones", "donaciones", "jdbc:mysql://localhost/bd_donaciones"));
-                        String nro_identificacion = request.getParameter("nro_identificacion");
-                        String nombres = request.getParameter("nombres");
-                        String apellido1 = request.getParameter("apellido1");
-                        String apellido2 = request.getParameter("apellido2");
-                        String usuario = request.getParameter("usuario");
-                        String correo = request.getParameter("correo");
-                        String pass = request.getParameter("pass");
-                        String perfil = request.getParameter("perfil");
-                        String estado = request.getParameter("estado");
-                        try {
-                            usuarioDAO.modificarUsuario(new Usuario(nro_identificacion, nombres, apellido1, apellido2, usuario, correo, pass, perfil, estado));
-                            request.setAttribute("mensaje", "El usuario fue modificado correctamente");
-                        } catch (Exception ex) {
-                            Logger.getLogger(Usuario.class.getName()).log(Level.SEVERE, null, ex);
-                            request.setAttribute("mensaje", ex.getMessage());
-                            request.setAttribute("nro_identificacion", nro_identificacion);
-                            request.setAttribute("nombres", nombres);
-                            request.setAttribute("apellido1", apellido1);
-                            request.setAttribute("apellido2", apellido2);
-                            request.setAttribute("usuario", usuario);
-                            request.setAttribute("correo", correo);
-                            request.setAttribute("pass", pass);
-                            request.setAttribute("perfil", perfil);
-                            request.setAttribute("estado", estado);
-                        }
-                        request.getRequestDispatcher("RegistrarUsuario.jsp").forward(request, response);
-                    } else {
-                        if (("Ingresar").equals(accion)) {
-                            UsuarioDAO usuarioDAO = new UsuarioDAO(new Conexion("dba_donaciones", "donaciones", "jdbc:mysql://localhost/bd_donaciones"));
-                            String user = request.getParameter("user");
-                            String pass = request.getParameter("pass");
-                            Usuario usuario;
-                            try {
-                                usuario = usuarioDAO.autenticar(user, pass);
-                                if (user.equals(usuario.getUsuario()) && pass.equals(usuario.getPassword())) {
-                                    System.out.println("entro");
-                                    request.getRequestDispatcher("Inicio.jsp").forward(request, response);
-                                } else {
-
-                                    System.out.println("No entro");
-                                    request.setAttribute("mensaje", "Usuario y/o contraseña incorrectos");
-                                    request.getRequestDispatcher("Login.jsp").forward(request, response);
-
-                                }
-                            } catch (Exception ex) {
-                                Logger.getLogger(Usuario.class.getName()).log(Level.SEVERE, null, ex);
-                                request.setAttribute("mensaje", " Usuario y/o contraseña incorrectos");
-                                request.getRequestDispatcher("Login.jsp").forward(request, response);
-                            }
-                        }
-                    }
-                }
+        } else if ("Consultar".equals(accion)) {
+            UsuarioDAO usuarioDAO = new UsuarioDAO(new Conexion("dba_donaciones", "donaciones", "jdbc:mysql://localhost/bd_donaciones"));
+            String user = request.getParameter("buscar_usu");
+            Usuario usuario;
+            try {
+                usuario = usuarioDAO.buscarUsuario(user);
+                request.setAttribute("usuario", user);
+                request.setAttribute("nro_identificacion", usuario.getNro_Identificacion());
+                request.setAttribute("nombres", usuario.getNombres());
+                request.setAttribute("apellido1", usuario.getApellido1());
+                request.setAttribute("apellido2", usuario.getApellido2());
+                request.setAttribute("usuario", usuario.getUsuario());
+                request.setAttribute("correo", usuario.getCorreo());
+                request.setAttribute("pass", usuario.getPassword());
+                request.setAttribute("pass2", usuario.getPassword());
+                request.setAttribute("perfil", usuario.getPerfil());
+                request.setAttribute("estado", usuario.getEstado());
+                request.setAttribute("mensaje", "El usuario fue encontrado");
+                request.getRequestDispatcher("RegistrarUsuario.jsp").forward(request, response);
+            } catch (Exception ex) {
+                Logger.getLogger(Usuario.class.getName()).log(Level.SEVERE, null, ex);
+                request.setAttribute("mensaje", "El usuario no fue encontrado");
+                request.getRequestDispatcher("RegistrarUsuario.jsp").forward(request, response);
             }
+        } else if ("Modificar".equals(accion)) {
+            UsuarioDAO usuarioDAO = new UsuarioDAO(new Conexion("dba_donaciones", "donaciones", "jdbc:mysql://localhost/bd_donaciones"));
+            String nro_identificacion = request.getParameter("nro_identificacion");
+            String nombres = request.getParameter("nombres");
+            String apellido1 = request.getParameter("apellido1");
+            String apellido2 = request.getParameter("apellido2");
+            String usuario = request.getParameter("usuario");
+            String correo = request.getParameter("correo");
+            String pass = request.getParameter("pass");
+            String perfil = request.getParameter("perfil");
+            String estado = request.getParameter("estado");
+            try {
+                usuarioDAO.modificarUsuario(new Usuario(nro_identificacion, nombres, apellido1, apellido2, usuario, correo, pass, perfil, estado));
+                request.setAttribute("mensaje", "El usuario fue modificado correctamente");
+            } catch (Exception ex) {
+                Logger.getLogger(Usuario.class.getName()).log(Level.SEVERE, null, ex);
+                request.setAttribute("mensaje", ex.getMessage());
+                request.setAttribute("nro_identificacion", nro_identificacion);
+                request.setAttribute("nombres", nombres);
+                request.setAttribute("apellido1", apellido1);
+                request.setAttribute("apellido2", apellido2);
+                request.setAttribute("usuario", usuario);
+                request.setAttribute("correo", correo);
+                request.setAttribute("pass", pass);
+                request.setAttribute("perfil", perfil);
+                request.setAttribute("estado", estado);
+            }
+            request.getRequestDispatcher("RegistrarUsuario.jsp").forward(request, response);
+        } else if ("Ingresar".equals(accion)) {
+            UsuarioDAO usuarioDAO = new UsuarioDAO(new Conexion("dba_donaciones", "donaciones", "jdbc:mysql://localhost/bd_donaciones"));
+            String user = request.getParameter("user");
+            String pass = request.getParameter("pass");
+            Usuario usuario;
+            try {
+                usuario = usuarioDAO.autenticar(user, pass);
+                if (user.equals(usuario.getUsuario()) && pass.equals(usuario.getPassword())) {
+                    System.out.println("entro");
+                    request.getRequestDispatcher("Inicio.jsp").forward(request, response);
+                } else {
+
+                    System.out.println("No entro");
+                    request.setAttribute("mensaje", "Usuario y/o contraseña incorrectos");
+                    request.getRequestDispatcher("Login.jsp").forward(request, response);
+
+                }
+            } catch (Exception ex) {
+                Logger.getLogger(Usuario.class.getName()).log(Level.SEVERE, null, ex);
+                request.setAttribute("mensaje", " Usuario y/o contraseña incorrectos");
+                request.getRequestDispatcher("Login.jsp").forward(request, response);
+            }
+        } else if ("Listar Usuarios".equals(accion)) {
+            System.out.println("Entro Listar");
+
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/bd_donaciones", "dba_donaciones", "donaciones");
+
+                ServletOutputStream servletOutputStream = response.getOutputStream();
+                File reportFile = new File(getServletConfig().getServletContext().getRealPath("WEB-INF/ReporteUsuario.jasper"));
+                byte[] bytes = null;
+
+                bytes = JasperRunManager.runReportToPdf(reportFile.getPath(), new HashMap(), connection);
+
+                response.setContentType("application/pdf");
+                response.setContentLength(bytes.length);
+
+                servletOutputStream.write(bytes, 0, bytes.length);
+                servletOutputStream.flush();
+                servletOutputStream.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            request.getRequestDispatcher("RegistrarUsuario.jsp").forward(request, response);
         }
+
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
